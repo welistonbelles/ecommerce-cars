d0e10b829f9bcb0a49261576152e2630ce96fe2e
welistonbelles <72521878+welistonbelles@users.noreply.github.com>
2022-02-16 09:21:53 -0300

    Alterado endpoint de documentação

--- config/urls.py
+++ config/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
 
 # swagger
 urlpatterns += [
-   url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   url(r'^doc(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   url(r'^doc/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
